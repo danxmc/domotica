a6a6ad3d36f0e564ad26fa58e46e48056116d0a5
danxmc <danx_97@hotmail.com>
2018-02-11 00:58:33 -0600

    Test after Auth branch merge

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -35,10 +35,10 @@ socket.on('toggleBtn', (data) => {
     console.log('listener:', data.status);
     console.log('listener:', data.btn);
     if (data.status == true) {
-        $('#'+data.btn).attr('aria-pressed', 'true');
-        $('#'+data.btn).addClass('active');
+        $('#' + data.btn).attr('aria-pressed', 'true');
+        $('#' + data.btn).addClass('active');
     } else {
-        $('#'+data.btn).attr('aria-pressed', 'false');
-        $('#'+data.btn).removeClass('active');
+        $('#' + data.btn).attr('aria-pressed', 'false');
+        $('#' + data.btn).removeClass('active');
     }
 });
