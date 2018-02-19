bf709a61742a20b3e52ea9b53e7fb589c74c28f6
danxmc <danx_97@hotmail.com>
2018-02-19 14:25:03 -0600

    Change colorPicker event to onChange

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,7 +1,7 @@
 // Make connection
 let socket = io.connect(window.location.hostname + ':' + 80);
 
-// Emit events
+// Event emitters
 //Light emitter event
 $(".lightBtn").on('click', (e) => {
     e.preventDefault;
@@ -17,35 +17,24 @@ $(".lightBtn").on('click', (e) => {
         btnNum: btn
     });
 });
+
 //RGB control event
-$(".RGBcontrol").on('click', (e) => {
+$("#colorPicker").on('change', (e) => {
     e.preventDefault;
 
-    let btnRGB = document.getElementById("colorpicker").value;
+    let btnRGB = document.getElementById("colorPicker").value;
     let hexvals = btnRGB.split("#");
     let hexval = hexvals[1].match(/.{1,2}/g);
     let color = "#" + (255 - (parseInt(hexval[0], 16))).toString(16) + (255 - (parseInt(hexval[1], 16))).toString(16) + (255 - (parseInt(hexval[2], 16))).toString(16);
 
-    //console.log('color: ' + color);
+    console.log('color: ' + color);
     socket.emit('RGBcontrol', {
         hex: color
-    })
-})
-/*
-$('#lightBtn').on('click', (e) => {
-    e.preventDefault;
-    let status;
-    // Check if button is currently active
-    $('#lightBtn').attr('aria-pressed') != 'true' ? status = true : status = false;
-    console.log('Button status:', status);
-    // Emit event to server with the according status
-    socket.emit('toggleLight', {
-        status: status
     });
 });
-*/
 
-// Listen for events
+// Event listeners
+// Light class button listener
 socket.on('toggleBtn', (data) => {
     console.log('listener status:', data.status);
     console.log('listener btn:', data.btnNum);
@@ -58,6 +47,7 @@ socket.on('toggleBtn', (data) => {
     }
 });
 
+// Input pin listener
 socket.on('inputEvent', (data) => {
     let dt = new Date();
     let time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
